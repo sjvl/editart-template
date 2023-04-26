ba68fe8cb071c48e4a6f4ff2075c8fbe4336ea90
Samuel Javelle <samueljavelle@hotmail.com>
2023-04-26 08:32:42 +0200

    test

--- index.js
+++ index.js
@@ -22,10 +22,13 @@ function drawArt() {
     ctx.fillStyle = `rgba(${randomM0() * 255}, ${randomM0() * 255}, ${
         randomM0() * 255
     }, 1)`;
-
     ctx.fillRect(0, 0, width, height);
 
     ctx.fillStyle = "rgba(0, 255, 0, 1)";
     ctx.fillRect(m1 * width, m2 * height, m3 * width * 0.5, m4 * height * 0.5);
+    ctx.fillRect(m1 * width, m2 * height, m3 * width * 0.5, m4 * height * 0.5);
+    ctx.fillRect(m1 * width, m2 * height, m3 * width * 0.5, m4 * height * 0.5);
+
+
     triggerPreview();
 }
